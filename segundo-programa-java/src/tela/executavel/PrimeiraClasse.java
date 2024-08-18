137789bc168a8ce34702f502c9e36705419f2ab9
LeoLBS <leonardoperin70@gmail.com>
2024-08-18 15:49:30 -0300

    feat: add segundo-programa-java

---
+++ segundo-programa-java/src/tela/executavel/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package tela.executavel;
+
+public class PrimeiraClasse {
+	
+	/*Main é um metodo que se auto executa*/
+	public static void main(String[] args) {
+		
+		/*Criando um texto no console*/
+		System.out.println("Meu Segundo programa em Java / Criado pelo Eclipse!");
+		System.out.println("Formação Curso Java EE (Web)!");
+	}
+}
