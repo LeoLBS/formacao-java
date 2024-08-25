c6523165b5b1707d1204e13db1dd8525f5c4c867
LeoLBS <leonardoperin70@gmail.com>
2024-08-25 08:49:53 -0300

    feat: add quinto-programa-java

---
+++ quinto-programa-java/src/tela/executar/QuintoPrograma.java
@@ -0,0 +1,15 @@
+package tela.executar;
+
+public class QuintoPrograma {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 20; i++) {
+			
+			System.out.println("");
+			
+			for(int x = 0; x != i; x++) {
+				System.out.print("*");
+			}
+		}
+	}
+}
