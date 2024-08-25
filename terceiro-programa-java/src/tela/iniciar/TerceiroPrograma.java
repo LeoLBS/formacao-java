348722eaf4a222d09d1af4f4d3d05986fdd5121e
LeoLBS <leonardoperin70@gmail.com>
2024-08-25 08:46:59 -0300

    feat: add terceiro-programa-java

---
+++ terceiro-programa-java/src/tela/iniciar/TerceiroPrograma.java
@@ -0,0 +1,25 @@
+package tela.iniciar;
+
+public class TerceiroPrograma {
+	public static void main(String[] args) {
+		
+		/*Tipos primitivos*/
+		byte tipoByte = 127;
+		short tipoShort = 32767;
+		int tipoInt = 2147483647;
+		long tipoLong = 922337203685477000l;
+		float tipoFloat = 2348333344339.8373447f;
+		double tipoDouble = 2324654586730933845835.494848484574765932;
+		char tipoChar = 'A';
+		boolean tipoBoolean = false;
+		
+		System.out.println("Byte: " + tipoByte);
+		System.out.println("Short: " + tipoShort);
+		System.out.println("Int: " + tipoInt);
+		System.out.println("Long: " + tipoLong);
+		System.out.println("Float: " + tipoFloat);
+		System.out.println("Double: " + tipoDouble);
+		System.out.println("Char: " + tipoChar);
+		System.out.println("Boolean: " + tipoBoolean);
+	}
+}
