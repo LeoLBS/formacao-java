d0cd84b6f233fafb98ab7f26a5c86b21205d8d8e
LeoLBS <leonardoperin70@gmail.com>
2024-08-26 13:32:03 -0300

    feat: add Colegio ( Criando uma nova classe Colegio e criando um objeto da classe Aluno)

---
+++ programa-classe/src/cursojava/escola/Colegio.java
@@ -0,0 +1,13 @@
+package cursojava.escola;
+
+//Importando o pacote onde esta minha classe Aluno
+import cursojava.classes.Aluno;
+
+public class Colegio {
+	public static void main(String[] args) {
+		
+		//Iniciando um objeto aluno1 da classe Aluno
+		Aluno aluno1 = new Aluno();
+	
+	}
+}
