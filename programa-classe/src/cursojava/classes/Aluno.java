06d782db74bd4d48fa6c41418cb1f151ed15ea60
LeoLBS <leonardoperin70@gmail.com>
2024-08-26 13:19:45 -0300

    feat: add programa-classe ( Criando a classe de Aluno )

---
+++ programa-classe/src/cursojava/classes/Aluno.java
@@ -0,0 +1,18 @@
+package cursojava.classes;
+
+import javax.sql.rowset.serial.SerialException;
+import javax.xml.crypto.Data;
+
+//Criar uma classe utilizar no singular, sendo a criação dessa classe o Aluno
+public class Aluno {
+
+	//Criando os atributos da classe Aluno
+	private String nomeString;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String classe;
+	private String dataMatricula;
+	private int registroAluno;
+	
+}
